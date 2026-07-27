6128ede80d85680e61eaf5c6b2c67b2cea32e933
Marc Chappellier <marc.chappellier@gmail.com>
2026-07-27 00:55:01 -0700

    Add new music

--- js/jukebox.js
+++ js/jukebox.js
@@ -10,7 +10,8 @@ const JUKEBOX_TRACKS = [
   { title: 'Underneath It All', artist: 'No Doubt', cover: 'assets/covers/underneath-it-all.jpg', audio: 'assets/audio/underneath-it-all.m4a' },
   { title: 'I Want to Break Free', artist: 'Queen', cover: 'assets/covers/i-want-to-break-free.jpg', audio: 'assets/audio/i-want-to-break-free.m4a' },
   { title: 'The Story', artist: 'Brandi Carlile', cover: 'assets/covers/the-story.jpg', audio: 'assets/audio/the-story.m4a' },
-  { title: 'Whenever, Wherever', artist: 'Shakira', cover: 'assets/covers/whenever-wherever.jpg', audio: 'assets/audio/whenever-wherever.m4a' }
+  { title: 'Whenever, Wherever', artist: 'Shakira', cover: 'assets/covers/whenever-wherever.jpg', audio: 'assets/audio/whenever-wherever.m4a' },
+  { title: 'Night Birds', artist: 'Shakatak', cover: 'assets/covers/night-birds.jpg', audio: 'assets/audio/night-birds.m4a' }
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
